20911a0058169201d2dad687037452727851ad0a
matthewryangibson99@gmail.com <matthewryangibson99@gmail.com>
2022-07-14 17:55:24 -0600

    updating js

--- script.js
+++ script.js
@@ -10,12 +10,16 @@ var c = document.getElementById("answer3");
 var d = document.getElementById("answer4");
 var timer = document.getElementById("countdown");
 var score = document.getElementById("scoreContainer");
-var quizQuestions = document.getElementById("answerContainer");
 var secondsLeft = 76; //the 75 doesnt display so i changed it to 76 but im sure thats not the right way to fix it
 var score = 0;
-
+var index = 0;
+button.addEventListener('click', playGame);
+a.addEventListener('click', nextQuestion)
+b.addEventListener('click', nextQuestion)
+c.addEventListener('click', nextQuestion)
+d.addEventListener('click', nextQuestion)
 //array containing questions answers and correct answer
-quizQuestions = [
+quizArray = [
     {
         question: "What is the name of the content you put inside of []?",
         answers: {
@@ -63,6 +67,72 @@ quizQuestions = [
  
 
 
+   
+    
+
+        
+    function setTime() {
+        
+        var timerInterval = setInterval(function() {
+          secondsLeft--;
+          timer.textContent = " " + secondsLeft;
+    
+          if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            //pop up asking for name and if youd like to record your score
+          }
+      
+        }, 1000);
+    }
+
+    
+
+function playGame() {
+    
+    setTime();
+    //array
+   var currentQuestion = quizArray[index];
+
+   var question = currentQuestion.question;
+   var choiceA = currentQuestion.answers.a;
+   var choiceB = currentQuestion.answers.b;
+   var choiceC = currentQuestion.answers.c;
+   var choiceD = currentQuestion.answers.d;
+
+    quizQuestions.textContent = question;
+    a.textContent = choiceA;
+    b.textContent = choiceB;
+    c.textContent = choiceC;
+    d.textContent = choiceD;
+    //objects
+    // string
+};
+    
+function nextQuestion(event) {
+    var user = event.target.textContent;
+    var correctAnswer = quizArray[0].correctAnswer;
+    index++;
+
+    //user click == correct answer, ++ score and move on to next question
+    //else -- score and - 5 seconds
+    if (correctAnswer == user) { 
+        score++;   
+    }else{
+        score--;
+        secondsLeft = secondsLeft -5;    
+    }
+
+    playGame();     
+};
+
+// if (correctAnswer) {
+//     score ++;
+// }else{
+//     score --;
+//     secondsLeft - 5;
+// }
+
+console.log(playGame);
 
 //functions
 //function for button that begins timer and questions
@@ -94,7 +164,7 @@ function setTime() {
 };
 
 
-console.log(playGame);
+
   
 
 
